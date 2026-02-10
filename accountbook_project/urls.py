04052c3705993a0e723fde92c78fc59f81d7e463
Nahyean Kim <kingnh0328@gmail.com>
2026-02-10 18:03:01 +0900

    presentation error fixed

--- accountbook_project/urls.py
+++ accountbook_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -10,7 +11,9 @@ urlpatterns = [
     path('transactions/', include('transactions.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('report/', include('report.urls')),
+    path('presentation/', TemplateView.as_view(template_name='presentation.html'), name='presentation'),
 ]
+
 
 
 if settings.DEBUG:
